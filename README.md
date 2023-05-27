b8f9e75c2a67f905bd247e3995fdf89f5117640d
Eduardo M Sala <edumunozsala@gmail.com>
2023-05-27 20:25:15 +0200

    Modified ingestion
    Sepatate read and chunking from embedding and vector database

--- README.md
+++ README.md
@@ -3,3 +3,14 @@
 
 ### From the Udemy Course "LangChain- Develop LLM powered applications with LangChain" 
 Chatbot Documentation
+
+## Download Lanchain Documentation
+Create a folder with the name "langchain-docs" and use wget to download the Langchain documentacion with the command:
+
+wget -r -A.html -P langchain-docs https://python.langchain.com/en/latest/
+!wget -r -A.html -P langchain-docs https://python.langchain.com/en/latest/
+wget -r --no-parent --html-extension -P langchain-docs https://python.langchain.com/en/latest/
+
+Try any of this commands.
+
+
--- ingestion.py
+++ ingestion.py
@@ -5,18 +5,36 @@ from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Pinecone
 import pinecone
 
-from consts import INDEX_NAME
+#from consts import INDEX_NAME
 
-pinecone.init(
-    api_key=os.environ["PINECONE_API_KEY"],
-    environment=os.environ["PINECONE_ENVIRONMENT_REGION"],
-)
+#pinecone.init(
+#    api_key=os.environ["PINECONE_API_KEY"],
+#    environment=os.environ["PINECONE_ENVIRONMENT_REGION"],
+#)
+
+def pinecone_embedding():
+    print(f"Going to insert {len(documents)} to Pinecone")
+    embeddings = OpenAIEmbeddings()
+    Pinecone.from_documents(documents[3969:], embeddings, index_name=INDEX_NAME)
+    print("****** Added to Pinecone vectorstore vectors")
 
 
+def chroma_embedding():
+    print(f"Going to insert {len(documents)} to Chroma")
+    embeddings = OpenAIEmbeddings()
+    db = Chroma.from_documents(documents[3969:], embeddings, index_name=INDEX_NAME)
+    #print("****** Added to Pinecone vectorstore vectors")
+    pass
+    
 def ingest_docs() -> None:
-    loader = ReadTheDocsLoader(path="langchain-docs/langchain.readthedocs.io/en/latest")
+    # Set the path to the documentation folder
+    doc_path= "langchain-docs\\langchain-docs\\python.langchain.com\\en\\latest"
+    # Create a loader with the documentation
+    loader = ReadTheDocsLoader(path=doc_path, encoding="ISO-8859-1")
+    # Load the documents
     raw_documents = loader.load()
     print(f"loaded {len(raw_documents) }documents")
+    
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=1000, chunk_overlap=100, separators=["\n\n", "\n", " ", ""]
     )
@@ -28,10 +46,10 @@ def ingest_docs() -> None:
         new_url = old_path.replace("langchain-docs", "https:/")
         doc.metadata.update({"source": new_url})
 
-    print(f"Going to insert {len(documents)} to Pinecone")
-    embeddings = OpenAIEmbeddings()
-    Pinecone.from_documents(documents[3969:], embeddings, index_name=INDEX_NAME)
-    print("****** Added to Pinecone vectorstore vectors")
+    #print(f"Going to insert {len(documents)} to Pinecone")
+    #embeddings = OpenAIEmbeddings()
+    #Pinecone.from_documents(documents[3969:], embeddings, index_name=INDEX_NAME)
+    #print("****** Added to Pinecone vectorstore vectors")
 
 
 if __name__ == "__main__":
